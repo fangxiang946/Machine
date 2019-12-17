7852ef42d442ec20906ee15ed2ce9bfefb1c4195
fangxiang <fangxiang@beisen.com>
2019-12-17 23:52:16 +0800

    tensorboard

--- notebook/启动命令/StartJupyter_home.bat
+++ notebook/启动命令/StartJupyter_home.bat
@@ -1,4 +1,4 @@
 @echo off
 d:
-cd d:\1.我要回珠海\0.实战项目\Machine\notebook
-C:\Users\fangxiang\Anaconda3\Scripts\jupyter-notebook.exe
+cd d:\我要回珠海\实战项目\Machine\notebook
+D:\python环境\learn\Scripts\jupyter-notebook.exe
